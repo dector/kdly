a383c9f46de54b4f94916f7ea31999eb34f8bb79
D <dector@dector.space>
2025-11-12 23:46:21 +0100

    feat: set parser structure

---
+++ pkg/parser/parser.go
@@ -0,0 +1,18 @@
+package parser
+
+// Parser represents a KDL v2 parser
+type Parser struct {
+	// TODO: Add parser fields
+}
+
+// New creates a new Parser instance
+func New() *Parser {
+	// TODO: Initialize parser
+	return &Parser{}
+}
+
+// Parse parses a KDL document from the provided string
+func (p *Parser) Parse(input string) error {
+	// TODO: Implement parsing logic
+	return nil
+}
---
+++ pkg/parser/parser_test.go
@@ -0,0 +1,9 @@
+package parser
+
+import "testing"
+
+func TestGoldenDataLocal(t *testing.T) {
+	t.Run("sample test", func(t *testing.T) {
+		// This test passes
+	})
+}
